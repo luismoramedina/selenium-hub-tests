ee33c43ab1ebdfacb9e0c316ef29934641b5949a
luismoramedina <luismoramedina@gmail.com>
2018-02-05 23:32:45 +0100

    parametrized tests, fix shared memory on ose

--- src/test/java/com/github/luismoramedina/GoogleSearchUnitTest.java
+++ src/test/java/com/github/luismoramedina/GoogleSearchUnitTest.java
@@ -1,8 +1,8 @@
 package com.github.luismoramedina;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,22 +12,39 @@ import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertTrue;
 
 /**
  * @author luismoramedina
  */
+@RunWith(Parameterized.class)
 public class GoogleSearchUnitTest {
+
 	private static final String HUB_URL = "http://localhost:4444/wd/hub";
-	private static final DesiredCapabilities CAPABILITIES = DesiredCapabilities.chrome();
 
 	private static RemoteWebDriver driver;
 	private static Wait<WebDriver> wait;
+	private String browser;
 
-	@BeforeClass
-	public static void setUp() throws Exception {
-		driver = new RemoteWebDriver(new URL(HUB_URL), CAPABILITIES);
+	public GoogleSearchUnitTest(String browser){
+		this.browser = browser;
+
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		DesiredCapabilities capabilities = null;
+		if (this.browser.equals("Chrome")) {
+			capabilities = DesiredCapabilities.chrome();
+		} else if (browser.equals("FF")) {
+			capabilities = DesiredCapabilities.firefox();
+		}
+
+		System.out.println("capabilities = " + capabilities);
+		driver = new RemoteWebDriver(new URL(HUB_URL), capabilities);
 		wait = new WebDriverWait(driver, 30);
 	}
 
@@ -43,10 +60,21 @@ public class GoogleSearchUnitTest {
 		assertTrue(ok);
 	}
 
-	@AfterClass
-	public static void tearDown() throws Exception {
-		driver.close();
-		driver.quit();
+	@After
+	public void tearDown() throws Exception {
+		try {
+			driver.close();
+			driver.quit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
+	@Parameterized.Parameters
+	public static Collection getBrowser(){
+		return Arrays.asList("FF", "Chrome");
+	}
+
+
 
 }
